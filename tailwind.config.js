1eb8f353e2da260833757fe2765f4db6f50a5c08
shun-inoue138 <ht8sk13@icloud.com>
2022-02-22 21:52:08 +0900

    chore:tailwind.config.jsにfont-familyクラスを拡張。bodyに反映

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: [
+          'Helvetica Neue',
+          'Arial',
+          'Hiragino Kaku Gothic ProN',
+          'Hiragino Sans',
+          'Meiryo',
+          'sans-serif',
+        ],
+        display: ['Oswald'],
+      },
+    },
   },
   plugins: [],
-}
+};
